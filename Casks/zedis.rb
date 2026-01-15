5dfd3d825d867d13d3b792b6e8bececb4aa99316
vicanso <tree.xie@outlook.com>
2026-01-15 21:31:50 +0800

    chore: version 0.1.8

--- Casks/zedis.rb
+++ Casks/zedis.rb
@@ -6,8 +6,8 @@ cask "zedis" do
 
   # 2. 分别定义两种架构的 SHA256
   # 格式：sha256 arm: "ARM哈希", intel: "Intel哈希"
-  sha256 arm:   "sha256:44eb9a2c7994a5c3a3df784e03687da59e026ce959ecd346014a51c25a7f9ad4",
-         intel: "sha256:758ed940c8fd1293e6fc3bb073a6133be484c1b94ad9d4a2f66503cf1f48282b"                            
+  sha256 arm:   "44eb9a2c7994a5c3a3df784e03687da59e026ce959ecd346014a51c25a7f9ad4",
+         intel: "758ed940c8fd1293e6fc3bb073a6133be484c1b94ad9d4a2f66503cf1f48282b"                            
 
   # 3. 动态 URL
   # #{arch} 会自动根据当前机器替换为 "aarch64" 或 "x86_64"
